d044363a8d06c813c12fb91694e0f6e10e540404
AnkitKumarvaid <61087993+AnkitKumarvaid@users.noreply.github.com>
2021-09-05 21:46:02 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
+const app = express();
+const db = require("./config/mongoose");
+
+app.use("/", require("./routes"));
+//start a server
+
+app.listen(port, () => console.log("server is up & running at port", port)); 

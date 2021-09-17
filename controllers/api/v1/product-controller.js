122a261b173cca4422b69c605f2322dd3255da92
AnkitKumarvaid <61087993+AnkitKumarvaid@users.noreply.github.com>
2021-09-17 15:46:56 +0530

    delete and update function update

--- controllers/api/v1/product-controller.js
+++ controllers/api/v1/product-controller.js
@@ -1,27 +1,87 @@
 const ProductModal=require("../../../modal/Product");
-const { validationResult } = require("express-validator");
+const { 
+    validationResult 
+} = require("express-validator");
  
 module.exports.createProduct=async(req,res)=>{
  try{
     const errors=validationResult(req);
     if(!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array() });
+        return res.status(400).json({
+            errors: errors.array() 
+        });
     }
+     
     const data =await ProductModal.create(req.body);
-    res.status(200).json({msg:"Product create Success", data: data})
+    res.status(200).json({
+        msg:"Product create Success", 
+        data: data
+    });
  }catch (error){
      console.log("Error in creating Product", error);
-     res.status(500).json({msg:"Internal Server Error"});
- }
+     res.status(500).json({
+         msg:"Internal Server Error"
+        });
+    }
 };
 
 module.exports.getProduct =async(req,res) => {
     try{
        const data =await ProductModal.find({});
-       res.status(200).json({msg:"Product create Success", data: data});
+       res.status(200).json({
+           msg:"Products", 
+           data: data
+        });
     }catch (error){
         console.log("Error in getting product", error);
-        res.status(500).json({msg:"Internal Server Error"});
+        res.status(500).json({
+            msg:"Internal Server Error"
+        });
+    }
+   }
+
+   module.exports.deleteProduct =async(req,res) => {
+    try{
+       const data =await ProductModal.deleteOne({});
+       res.status(200).json({
+           msg:"Products", 
+           data: data
+        });
+    }catch (error){
+        console.log("Error in getting product", error);
+        res.status(500).json({
+            msg:"Internal Server Error"
+        });
     }
    };
 
+
+   module.exports.updateProduct = async (req, res) => {
+    try {
+        const data = await ProductModal.findByIdAndUpdate(
+            req.params["id"],
+            req.body, {
+                new: true
+            }
+        );
+
+        if (!data) {
+            res
+                .status(400)
+                .json({
+                    msg: "NOT FOUND",
+                    data: "Item with id not found."
+                });
+        } else {
+            res.status(200).json({
+                msg: "UPDATE SUCCESS",
+                data: data
+            });
+        }
+    } catch (error) {
+        console.error("Error in updating product", error);
+        res.status(500).json({
+            msg: "Internal Server Error"
+        });
+    }
+};
--- routes/api/v1/product.js
+++ routes/api/v1/product.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const { body } = require("express-validator");
+const { 
+    body,
+    checkSchema
+ } = require("express-validator");
 const productController=require('../../../controllers/api/v1/product-controller');
 
 router.post(
     '/create',
 [
     body("name").not().isEmpty().withMessage("Name is Empty"),
-    body("quantity")
-        .isFloat ({gt: 0})
-        .withMessage("quantity must be greater than 0")
-], 
-productController.createProduct
-);
+    body("quantity").isFloat ({
+        gt: 0
+    }).withMessage("quantity must be greater than 0"),
+    body("description").not().isEmpty().withMessage("Description is Empty"),
+    body("price").isFloat({
+        gt: 0
+    }).withMessage("Price is less than 0")
+
+], productController.createProduct);
+
 router.get("/all",productController.getProduct);
+
+router.patch("/update/:id", checkSchema({
+    id: {
+        in: ["params"],
+        errorMessage: "ID is wrong",
+        isMongoId: true,
+    }
+}), productController.updateProduct);
+
+router.delete("/delete", productController.deleteProduct);
 module.exports = router;
